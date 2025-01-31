5190cc11501d6765f891ef31b0c3084e21dc8115
Roma Golubev <romag643@gmail.com>
2025-01-31 17:47:45 +0300

    kek

--- src/problem_data.h
+++ src/problem_data.h
@@ -1,7 +1,11 @@
 #include "tool.h"
+#include "work.h"
+
 
 class ProblemData {
 public:
+    ProblemData(std::istream&& input);
 private:
     std::vector<Tool> tools_;
+    std::vector<Work> works_;
 }
